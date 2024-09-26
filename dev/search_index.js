1010765087ba4177c1a2dad14f3a229c6b20e939
Documenter.jl <documenter@juliadocs.github.io>
2024-09-26 14:45:03 +0000

    build based on 1055dab

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NumericalToolKits","category":"page"},{"location":"#NumericalToolKits","page":"Home","title":"NumericalToolKits","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NumericalToolKits.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NumericalToolKits]","category":"page"}]
+}
